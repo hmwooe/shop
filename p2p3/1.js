74c82f93f90661366a574095dc2a1ab6794d8986
hmwooe <961278309@qq.com>
2016-12-07 00:01:23 +0800

    第三次的代码

---
+++ p2p3/1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wooe on 2016/12/4.
+ */
+// function apple(a,b) {
+//     if(a==1){
+//         console.log(b)
+//         return b;
+//     }
+//     return apple(a-1,(b+1)*2);
+// }
+// apple(10,1);
+
+
+// function fn(n){
+//     if(n==10){
+//         return 1;
+//     }
+//     return 2*fn(n+1)+2
+// }
